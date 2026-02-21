c732bb0e6f8d2390ea2c2280ecd680185610ecda
Harrison Mbemba <hmbemba@gmail.com>
2026-02-20 19:54:07 -0500

    Update

--- rz.nimble
+++ rz.nimble
@@ -1,4 +1,4 @@
-version     = "0.1.0"
+version       = "0.1.1" 
 author      = "Harrison Mbemba"
 description = "rz: small Result/Rz type + safe helpers (native + JS), ergonomic combinators and utilities."
 license     = "MIT"
--- src/rz/macros.nim
+++ src/rz/macros.nim
@@ -1,25 +1,20 @@
 import rz/core
 
+template retType(): untyped =
+    when compiles(typeof(result).T):
+        typeof(result).T
+    else:
+        typeof(result)
+
 template `?`*[T, E](r: Rz[T, E]): untyped =
-    ## Early-return helper.
-    ##
-    ## Works inside procs that return Rz[SomeT, E].
-    ## If `r` is Err -> returns Err from the current proc.
-    ## If Ok -> yields the inner value.
     block:
         let tmp = r
         if not tmp.ok:
-            return errAs(default(typeof(result)), tmp.err)
+            return errAs(default(retType()), tmp.err)
         tmp.val
 
+
 template catch*[T, E](r: Rz[T, E], body: untyped): untyped =
-    ## Evaluate Rz. If Err, inject `it` and run `body`.
-    ## If Ok, yields the value.
-    ##
-    ## Example:
-    ##   let n = catch(accessInt("nope")):
-    ##       echo it.err
-    ##       return err[string](it.err)
     block:
         let tmp = r
         if not tmp.ok:
